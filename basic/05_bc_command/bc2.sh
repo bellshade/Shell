93c21f7c5a35a8565ae703e36bd26370ddd9387f
ItsArul <nextarituenak01@gmail.com>
2022-01-17 15:07:22 +0000

    menambahkan materi bc command

---
+++ basic/05_bc_command/bc2.sh
@@ -0,0 +1,6 @@
+echo "num=50;num+=20;num" | bc
+echo "num=50;num-=20;num" | bc
+echo "num=50;num*=20;num" | bc
+echo "num=50;num/=20;num" | bc
+echo "num=50;num^=20;num" | bc
+echo "num=50;num%=20;num" | bc
