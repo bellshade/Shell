9492a45e5694c158dab1a38fca0926fbeffd9b9c
slowy07 <slowy.arfy@gmail.com>
2022-01-23 20:43:25 +0700

    fix: testing
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- test.sh
+++ test.sh
@@ -2,11 +2,5 @@
 
 for nama_folder in basic; do
   echo "$nama_folder"
-  # menjalankan semua file shell
-  for file_shell in basic/*/*.sh; do
-    printf "\n"
-    echo "$file_shell"
-    sh "$file_shell"
-  done
 done
 
